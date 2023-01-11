1ab1d8e5742ce155f2cc815bf5357f423c84a97c
achaukin <achaukin@netw.fr>
2023-01-11 20:02:44 +0200

    worker init

---
+++ worker/index.js
@@ -0,0 +1,15 @@
+const {redisHost, redisPort} = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: redisHost,
+    port: redisPort,
+    retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+function fib (index) {
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
